6107514c890a9ca25c4c155f16f52f6a3f11b40a
Jeison <110923804+JeisonOJ@users.noreply.github.com>
2024-07-08 08:26:36 -0500

    Update DataService.java

--- database/DataService.java
+++ database/DataService.java
@@ -3,6 +3,10 @@ package database;
 public class DataService {
 
   private IDataStorage dataStorage;
+
+  public DataService(){
+    dataStorage = new IDataStorage();
+  }
   
   public void saveData(String data){
     dataStorage.saveData(data);
